b5b79d77c936edb50e028b563777e2b9c2a91a67
Juan Baltazar <juan.baltazar1@gmail.com>
2020-06-04 17:28:32 -0700

    Updates

--- precache-manifest.2b28d3ca134b0ee1533aa3c4357610b4.js
+++ precache-manifest.2b28d3ca134b0ee1533aa3c4357610b4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bd084d385bdc84894465d184c4aeb135",
+    "revision": "58b194cdefc7b0a71fc173dfb4ab9f50",
     "url": "/gameOfLife/index.html"
   },
   {
-    "revision": "34162c2c1cb9460a8e12",
-    "url": "/gameOfLife/static/css/main.38c40410.chunk.css"
+    "revision": "bfa7a7f4a98665c542eb",
+    "url": "/gameOfLife/static/css/main.6291c8f7.chunk.css"
   },
   {
     "revision": "a61f1b0af3123b209137",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/gameOfLife/static/js/2.244f0fb0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "34162c2c1cb9460a8e12",
-    "url": "/gameOfLife/static/js/main.f316c092.chunk.js"
+    "revision": "bfa7a7f4a98665c542eb",
+    "url": "/gameOfLife/static/js/main.71b7e4de.chunk.js"
   },
   {
     "revision": "ee09a42b7ea3cfe5907f",
